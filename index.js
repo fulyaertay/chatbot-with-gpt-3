a6307b8d19d2f4250100d01dc3123b5e762506bb
Fulya <fulya.ertay@gmail.com>
2023-06-17 13:50:34 +0300

    added frequency_penalty

--- index.js
+++ index.js
@@ -30,15 +30,28 @@ document.addEventListener('submit', (e) => {
     newSpeechBubble.textContent = userInput.value
     userInput.value = ''
     chatbotConversation.scrollTop = chatbotConversation.scrollHeight
-})
+}) 
 
 async function fetchReply(){
     const response = await openai.createChatCompletion({
         model: 'gpt-4',
-        messages: conversationArr  
-    })
+        messages: conversationArr,
+        presence_penalty: 0,
+        frequency_penalty: 0.3
+/*
+Challenge:
+    1. Set the frequency_penalty to 0.
+    2. Give the chatbot this query: Generate 20 ways to 
+       say "you can't buy that because you're broke".
+    3. Paste the results into output.md.
+    4. Repeat the process with frequency_penalty set to 2.
+    7. Examine the differences between the 2 outputs.
+    ⚠️ DO NOT SET frequency_penalty to -2!!!
+*/
+    }) 
     conversationArr.push(response.data.choices[0].message)
-    renderTypewriterText(response.data.choices[0].message.content)
+    // renderTypewriterText(response.data.choices[0].message.content)
+    console.log(response.data.choices[0].message.content)
 }
 
 function renderTypewriterText(text) {
