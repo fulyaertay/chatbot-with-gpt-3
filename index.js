f223810b3b12bcdccc876a258d5e0c999dcba387
Fulya <fulya.ertay@gmail.com>
2023-06-17 14:26:38 +0300

    added startover button

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getDatabase, ref } from 'firebase/database'
+import { getDatabase, ref, push, get, remove } from 'firebase/database'
 
 import { Configuration, OpenAIApi } from 'openai'
 import { process } from './env'
@@ -11,7 +11,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 const appSettings = {
-    databaseURL: 'https://chatbot-b289a-default-rtdb.firebaseio.com/'
+    databaseURL: 'https://knowitall-openai-default-rtdb.europe-west1.firebasedatabase.app/'
 }
 
 const app = initializeApp(appSettings)
@@ -21,18 +21,16 @@ const database = getDatabase(app)
 const conversationInDb = ref(database)
 
 const chatbotConversation = document.getElementById('chatbot-conversation')
- 
-const conversationArr = [
-    {
-        role: 'system',
-        content: 'You are an assistant that gives very short answers.'
-    } 
-] 
- 
+
+const instructionObj = {
+    role: 'system',
+    content: 'You are a helpful assistant.'
+}
+
 document.addEventListener('submit', (e) => {
     e.preventDefault()
-    const userInput = document.getElementById('user-input')   
-    conversationArr.push({ 
+    const userInput = document.getElementById('user-input')
+    push(conversationInDb, {
         role: 'user',
         content: userInput.value
     })
@@ -43,17 +41,27 @@ document.addEventListener('submit', (e) => {
     newSpeechBubble.textContent = userInput.value
     userInput.value = ''
     chatbotConversation.scrollTop = chatbotConversation.scrollHeight
-}) 
+})
 
-async function fetchReply(){
-    const response = await openai.createChatCompletion({
-        model: 'gpt-4',
-        messages: conversationArr,
-        presence_penalty: 0,
-        frequency_penalty: 0.3
-    }) 
-    conversationArr.push(response.data.choices[0].message)
-    renderTypewriterText(response.data.choices[0].message.content)
+function fetchReply() {
+    get(conversationInDb).then(async (snapshot) => {
+        if (snapshot.exists()) {
+            const conversationArr = Object.values(snapshot.val())
+            conversationArr.unshift(instructionObj)
+            const response = await openai.createChatCompletion({
+                model: 'gpt-4',
+                messages: conversationArr,
+                presence_penalty: 0,
+                frequency_penalty: 0.3
+            })
+            push(conversationInDb, response.data.choices[0].message)
+            renderTypewriterText(response.data.choices[0].message.content)
+        }
+        else {
+            console.log('No data available')
+        }
+
+    })
 }
 
 function renderTypewriterText(text) {
@@ -62,7 +70,7 @@ function renderTypewriterText(text) {
     chatbotConversation.appendChild(newSpeechBubble)
     let i = 0
     const interval = setInterval(() => {
-        newSpeechBubble.textContent += text.slice(i-1, i)
+        newSpeechBubble.textContent += text.slice(i - 1, i)
         if (text.length === i) {
             clearInterval(interval)
             newSpeechBubble.classList.remove('blinking-cursor')
@@ -71,3 +79,27 @@ function renderTypewriterText(text) {
         chatbotConversation.scrollTop = chatbotConversation.scrollHeight
     }, 50)
 }
+
+document.getElementById('clear-btn').addEventListener('click', () => {
+    remove(conversationInDb)
+    chatbotConversation.innerHTML = '<div class="speech speech-ai">How can I help you?</div>'
+})
+
+function renderConversationFromDb(){
+    get(conversationInDb).then(async (snapshot)=>{
+        if(snapshot.exists()) {
+            Object.values(snapshot.val()).forEach(dbObj => {
+                const newSpeechBubble = document.createElement('div')
+                newSpeechBubble.classList.add(
+                    'speech',
+                    `speech-${dbObj.role === 'user' ? 'human' : 'ai'}`
+                    )
+                chatbotConversation.appendChild(newSpeechBubble)
+                newSpeechBubble.textContent = dbObj.content
+            })
+            chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+        }
+    })
+}
+
+renderConversationFromDb()
