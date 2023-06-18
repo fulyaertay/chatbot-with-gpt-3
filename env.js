02b89209be1692f02925b055b1f35b624832c60b
Fulya <fulya.ertay@gmail.com>
2023-06-18 12:53:38 +0300

    added api key

--- env.js
+++ env.js
@@ -1,7 +1,6 @@
 //api key not working, please add your own key
 export const process = {
     env: {
-        //api key is not used please add your own key
         OPENAI_API_KEY: "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"
     }
 }
