d4a6593676597d723de1aaac919b4b4fc7a29d61
Fulya <fulya.ertay@gmail.com>
2023-06-21 13:15:02 +0300

    submit message testing added

--- cypress/e2e/2-advanced-examples/openPage.cy.js
+++ cypress/e2e/2-advanced-examples/openPage.cy.js
@@ -6,6 +6,7 @@ context('openPage', () => {
 
       cy.visit('https://chatbot-w-gpt-3.netlify.app/')
       cy.get('#user-input').type("how can i learn javascript?")
+      cy.get('#submit-btn').click()
     })
 }
 )
