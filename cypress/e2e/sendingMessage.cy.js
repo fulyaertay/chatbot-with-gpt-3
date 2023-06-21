dca55950f6158f80cc2263f6a236554c2ed93171
Fulya <fulya.ertay@gmail.com>
2023-06-21 13:47:26 +0300

    comments added

--- cypress/e2e/sendingMessage.cy.js
+++ cypress/e2e/sendingMessage.cy.js
@@ -3,13 +3,13 @@
 context('Sending message to OpenAI', () => {
    
     it.only('Sending message to OpenAI', () => {
-      //open web page
+      //1-open web page
       cy.visit('https://chatbot-w-gpt-3.netlify.app/')
-      // typing on input box
+      //2-typing on input box
       cy.get('#user-input').type("how can i learn javascript?")
-      //sending message to open ai
+      //3-sending message to open ai
       cy.get('#submit-btn').click()
-      //clear all conversations
+      //4-clear all conversations
       cy.get('#clear-btn').click()
     })
 }
