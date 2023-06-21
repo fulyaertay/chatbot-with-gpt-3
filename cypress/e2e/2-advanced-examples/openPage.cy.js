6c28e370505de20fbefa6c4d337c2c645f4ba676
Fulya <fulya.ertay@gmail.com>
2023-06-21 13:16:36 +0300

    comments added

--- cypress/e2e/2-advanced-examples/openPage.cy.js
+++ cypress/e2e/2-advanced-examples/openPage.cy.js
@@ -3,10 +3,13 @@
 context('openPage', () => {
    
     it('Open Home Page', () => {
-
+      //open web page
       cy.visit('https://chatbot-w-gpt-3.netlify.app/')
+      // typing on input box
       cy.get('#user-input').type("how can i learn javascript?")
+      //sending message to open ai
       cy.get('#submit-btn').click()
+      //clear all conversations
       cy.get('#clear-btn').click()
     })
 }
